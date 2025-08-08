d25d0630564acd6536a909435e2861490b29cf67
mateuszkaczmarczyk <mateusz.kaczmarczyk@tylko.com>
2025-08-08 21:38:04 +0200

    damage system update with enemy and player damaging logic

--- src/Systems/DamageSystem.hpp
+++ src/Systems/DamageSystem.hpp
@@ -3,9 +3,11 @@
 
 #include "../ECS/ECS.hpp"
 #include "../Events/EventBus.hpp"
+#include "../Events/CollisionEvent.hpp"
 #include "../Components/TransformComponent.hpp"
 #include "../Components/BoxColliderComponent.hpp"
-#include "../Events/CollisionEvent.hpp"
+#include "../Components/HealthComponent.hpp"
+#include "../Components/ProjectileComponent.hpp"
 
 class DamageSystem: public System {
 public:
@@ -20,8 +22,41 @@ public:
     }
 
     void OnCollision(CollisionEvent &event) {
-        // event.entityA.Kill();
-        // event.entityB.Kill();
+        auto& entityA = event.entityA;
+        auto& entityB = event.entityB;
+
+        if (entityA.HasGroup("Projectiles") && entityB.HasTag("Player")) {
+            OnPlayerDamaged(entityB, entityA);
+        }
+        if (entityA.HasGroup("Projectiles") && entityB.HasGroup("Enemies")) {
+            OnEnemyDamaged(entityB, entityA);;
+        }
+        if (entityB.HasGroup("Projectiles") && entityA.HasTag("Player")) {
+            OnPlayerDamaged(entityA, entityB);
+        }
+        if (entityB.HasGroup("Projectiles") && entityA.HasGroup("Enemies")) {
+            OnEnemyDamaged(entityA, entityB);
+        }
+    }
+
+    void OnPlayerDamaged(Entity& playerEntity, Entity& projectileEntity) {
+        auto& projectile = projectileEntity.GetComponent<ProjectileComponent>();
+        if (projectile.affectable) {
+            auto& health = playerEntity.GetComponent<HealthComponent>();
+            health.healthPercentage -= projectile.damage;
+            projectileEntity.Kill();
+            if (health.healthPercentage < 0) playerEntity.Kill();
+        }
+    }
+
+    void OnEnemyDamaged(Entity& enemyEntity, Entity& projectileEntity) {
+        auto& projectile = projectileEntity.GetComponent<ProjectileComponent>();
+        if (!projectile.affectable) {
+            auto& health = enemyEntity.GetComponent<HealthComponent>();
+            health.healthPercentage -= projectile.damage;
+            projectileEntity.Kill();
+            if (health.healthPercentage < 0) enemyEntity.Kill();
+        }
     }
 
     void Update() {
